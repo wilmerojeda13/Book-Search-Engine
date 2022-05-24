6628dcfb887932bfaee7499c582e1bce7da24545
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-23 21:38:15 -0400

    exporting apollo client and starting a function for apollo client thats store date and their particular token by its id

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {ApolloProvider} from '@apollo/react-hooks'
+import {ApolloClient} from 'apollo-boost'
+
+//start function for apollo client thats store date and their particular token by its id
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: 'graphql',
+})
+
 
 function App() {
   return (
