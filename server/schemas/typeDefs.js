342cc9b3c3ecc71c8118ec36c9796ed53456885d
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-05-27 16:31:21 -0400

    trying to fix schema to try to fix an error that I was having with the sign up form

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,8 +4,8 @@ const { gql } = require('apollo-server-express');
 //Creating custom tags to retrieve array for al book data with the 'Type argument
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     bookCount: Int
     savedBooks: [Book]
@@ -24,8 +24,8 @@ const typeDefs = gql`
     user: User
   }
 
-  input bookInput {
-    bookId: String
+  input BookInput {
+    bookId: String!
     authors: [String]
     description:  String
     title: String
@@ -34,10 +34,10 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String): Auth
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
-    removeBook(bookId: String!): User
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
   }
   type Query {
     me: User
